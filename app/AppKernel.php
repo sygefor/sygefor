4c6c0485735d44833e5a9db58dbf04ae597c51e8
Erwan <erwan@conjecto.com>
2016-10-10 14:31:13 +0200

    update sf version to 2.8

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,16 +8,12 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            // priority project bundles
-            new Sygefor\Bundle\ElasticaBundle\SygeforElasticaBundle(),
-
             // Core
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
@@ -39,23 +35,23 @@ class AppKernel extends Kernel
             new KULeuven\ShibbolethBundle\ShibbolethBundle(),
 
             // project bundles
+            new Sygefor\Bundle\TrainingBundle\SygeforTrainingBundle(),
             new Sygefor\Bundle\CoreBundle\SygeforCoreBundle(),
             new Sygefor\Bundle\ApiBundle\SygeforApiBundle(),
-            new Sygefor\Bundle\UserBundle\SygeforUserBundle(),
-            new Sygefor\Bundle\TaxonomyBundle\SygeforTaxonomyBundle(),
-            new Sygefor\Bundle\TrainingBundle\SygeforTrainingBundle(),
-            new Sygefor\Bundle\ListBundle\SygeforListBundle(),
             new Sygefor\Bundle\TraineeBundle\SygeforTraineeBundle(),
+            new Sygefor\Bundle\InscriptionBundle\SygeforInscriptionBundle(),
+            new Sygefor\Bundle\InstitutionBundle\SygeforInstitutionBundle(),
             new Sygefor\Bundle\TrainerBundle\SygeforTrainerBundle(),
-            new Sygefor\Bundle\ElasticaUpdateBundle\SygeforElasticaUpdateBundle(),
-            new Sygefor\Bundle\LheoBundle\SygeforLheoBundle(),
-            new Sygefor\Bundle\ActivityReportBundle\SygeforActivityReportBundle(),
+            new Sygefor\Bundle\MyCompanyBundle\SygeforMyCompanyBundle(),
+            new \Sygefor\Bundle\ActivityReportBundle\SygeforActivityReportBundle(),
 
             // keep this extension at the end to ensure listeners registring order
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle()
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Sygefor\Bundle\FrontBundle\SygeforFrontBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
@@ -66,6 +62,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
